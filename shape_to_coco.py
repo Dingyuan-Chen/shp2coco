acc85ea7e75ec3260e4149fdf5eabb6faa313402
DuncanChen <37899464+DuncanChen2018@users.noreply.github.com>
2019-04-21 12:46:52 +0800

    Update shape_to_coco.py

--- shape_to_coco.py
+++ shape_to_coco.py
@@ -53,10 +53,11 @@ def filter_for_annotations(root, files, image_filename):
     file_types = ['*.tif']
     file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
     basename_no_extension = os.path.splitext(os.path.basename(image_filename))[0]
-    file_name_prefix = basename_no_extension + '.*'
+#     file_name_prefix = basename_no_extension + '.*'
     files = [os.path.join(root, f) for f in files]
     files = [f for f in files if re.match(file_types, f)]
-    files = [f for f in files if re.match(file_name_prefix, os.path.splitext(os.path.basename(f))[0])]
+#     files = [f for f in files if re.match(file_name_prefix, os.path.splitext(os.path.basename(f))[0])]
+    files = [f for f in files if basename_no_extension == os.path.splitext(os.path.basename(f))[0].split('_', 1)[0]]
 
     return files
 
